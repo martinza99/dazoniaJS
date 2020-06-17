2a8a2c055aaecbcd859060946856582c8868e8a8
martinza99 <mrt.zacharias@gmail.com>
2020-06-17 16:31:28 +0200

    added files endpoint

--- api/routes/file.js
+++ api/routes/file.js
@@ -2,19 +2,19 @@ const express = require("express");
 const config = require("../config/config.js");
 const router = express.Router();
 const con = require("../config/database");
+const file = require("../models/file");
+const fs = require("fs");
+const ratingRouter = require("./rating");
 
-router.get("/:id", async (req, res) => {
-	try {
-		const sql = `SELECT * FROM file WHERE filename = ?`;
-		const results = (await con.promise().query(sql, req.params.id))[0];
-		if (results.length > 0)
-			res.json(results[0]);
-		else
-			res.status(404).json({ status: 404, message: "File not found" });
-	} catch (error) {
-		res.status(500).json({ status: 500, message: "An SQL error occured" });
-		throw error;
-	}
+router.use("/:id/ratings", checkFile, ratingRouter);
+
+router.get("/search", async (req, res) => {
+	//TODO
+	res.send("done");
+});
+
+router.get("/:id", checkFile, async (req, res) => {
+	res.json(req.file);
 });
 
 router.get("/", async (req, res) => {
@@ -23,31 +23,59 @@ router.get("/", async (req, res) => {
 		const results = (await con.promise().query(sql))[0];
 		res.json(results);
 	} catch (error) {
-		res.statusCode = 500;
-		res.statusMessage = "An SQL error occured"
-		res.json({ status: 500, message: "An SQL error occured" })
+		res.sendStatus(500);
 		throw error;
 	}
 });
 
-router.delete("/:id", async(req, res)=>{
-	//TODO
-	res.status(204);
+router.delete("/:id", checkFile, async (req, res) => {
+	if (req.user.isAdmin || req.user.userID == req.file.userId) {
+		const promises = [];
+		try {
+			promises.push(con.promise().query("DELETE FROM file WHERE fileID = ?", req.file.fileID));
+			promises.push(fs.promises.unlink(`${__dirname}/../../uploads/files/${req.file.filename}`));
+			promises.push(fs.promises.unlink(`${__dirname}/../../uploads/thumbnails/${req.file.filename}`));
+			await Promise.all(promises);
+			res.sendStatus(204);
+		} catch (error) {
+			res.sendStatus(500);
+			throw error;
+		}
+	}
+	else
+		res.status(403).json({ status: 403, message: `No permission to delete file` })
 });
 
-router.post("/", async(req, res)=>{
-	console.log(req.body);
+router.post("/", async (req, res) => {
+	//TODO
 	res.send("done");
 });
 
-router.patch("/:id/title", async(req, res)=>{
-	//TODO
-	console.log(req);
-	const sql = `UPDATE file SET title = ? WHERE filename = ?`;
-	const results = (await con.promise().query(sql, [req.body.title, req.params.id]))[0];
-	res.json(results);
+router.patch("/:id/title", checkFile, async (req, res) => {
+	if (req.user.isAdmin || req.user.userID == req.file.userId) {
+		try {
+			const sql = `UPDATE file SET title = ? WHERE fileID = ?`;
+			await con.promise().query(sql, [req.body.title, req.file.fileID]);
+			req.file.title = req.body.title;
+			res.json(req.file);
+		} catch (error) {
+			res.sendStatus(500);
+			throw error;
+		}
+	}
+	else
+		res.status(403).json({ status: 403, message: `No permission to edit this file` })
+
 });
 
-
-
 module.exports = router;
+
+async function checkFile(req, res, next) {
+	try {
+		const f = await file.getByName(req.params.id);
+		req.file = f;
+		next();
+	} catch (error) {
+		res.status(404).json({ status: 404, message: "File not found" });
+	}
+}
