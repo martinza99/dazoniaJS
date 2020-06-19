a6a105a60f4d657a6c61916bd21fc15a3f98f8ed
martinza99 <mrt.zacharias@gmail.com>
2020-06-19 21:58:06 +0200

    removed redirects from auth routes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,15 +3,18 @@ const config = require("../config/config.js");
 const passport = require("passport");
 const router = express.Router();
 
-router.post("/login", passport.authenticate("local",
-	{
-		successRedirect: "/"
-	}
-));
+router.post("/login", passport.authenticate("local"), (req, res) => {
+	res.status(200).json(req.user);
+});
 
-router.post("/logout", (req, res)=>{
+router.post("/logout", (req, res) => {
 	req.logout();
-	res.redirect("/auth/login");
+	res.sendStatus(204);
+});
+
+router.post("/resetPassword", (req, res) => {
+	//todo
+	res.sendStatus(501);
 });
 
 module.exports = router;
